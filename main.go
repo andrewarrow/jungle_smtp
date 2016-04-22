6dc080e377674de05f2b83562518a0276af6a416
Andrew Arrow <andrew@0x7a69.net>
2016-04-22 16:37:56 -0700

    file

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "net"
 import "fmt"
 import "bufio"
 import "time"
+import "os"
+import "io"
 
 type Client struct {
 	conn    net.Conn
@@ -20,24 +22,43 @@ func (c *Client) w(s string) {
 func (c *Client) r() string {
 	reply, err := c.bufin.ReadString('\n')
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("e ", err)
 	}
 	return reply
+}
+
+func appendToFile(text string) {
+	of, _ := os.OpenFile("incoming_emails.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
+	defer of.Close()
+	of.Seek(0, os.SEEK_END)
+	io.WriteString(of, text+"\n")
 }
 
 func handleClient(c *Client) {
 	fmt.Println("here")
 	c.w("220 Welcome to the Jungle")
 	text := c.r()
+	appendToFile(text)
 	c.w("250 No one says helo anymore")
 	text = c.r()
+	appendToFile(text)
 	c.w("250 Sender")
 	text = c.r()
+	appendToFile(text)
 	c.w("250 Recipient")
 	text = c.r()
+	appendToFile(text)
 	c.w("354 Ok Send data ending with <CRLF>.<CRLF>")
-	text = c.r()
-	fmt.Println(text)
+	for {
+		text = c.r()
+		bytes := []byte(text)
+		appendToFile(text)
+		// 46 13 10
+		if bytes[0] == 46 && bytes[1] == 13 && bytes[2] == 10 {
+			break
+		}
+	}
+	c.conn.Close()
 }
 
 func main() {
